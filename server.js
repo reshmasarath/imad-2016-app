0088643100c64c5a81832fc32e770ad4d4f678b5
reshmasarath <reshmasarathk97@gmail.com>
2016-09-20 17:49:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/',"article one", function (req, res) {
+app.get('/',"article-one", function (req, res) {
   res.send("article one.html");
   
 });
-app.get('/',"article two", function (req, res) {
+app.get('/',"article-two", function (req, res) {
   res.send("article two will be served here File");
   
 });
-app.get('/',"article three", function (req, res) {
+app.get('/',"article-three", function (req, res) {
   res.send("article three will be served here");});
   
 
