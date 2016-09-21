d6aa3fdcc603a485ae237d48ce816efafc27045d
reshmasarath <reshmasarathk97@gmail.com>
2016-09-21 20:20:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,18 +5,12 @@ var path = require('path');
 var app  = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(_dirname,'article0ne.html'));
-  
-});
-app.get('/',"article-two", function (req, res) {
-  res.send("article two will be served here File");
-  
-});
-app.get('/',"article-three", function (req, res) {
+app.get('/', function (req, res)
+{  res.sendFile(path.join(__dirname, 'ui', 'index.html'));});
+app.get('/article-one', function(req,res) {res.sendFile(path.join(__dirname,'ui','article-one.html'))});
+app.get('/article-two', function (req, res) {
+  res.send("article two will be served here");});
+app.get('/article-three', function (req, res) {
   res.send("article three will be served here");});
   
 
